5edace4cee97a0d0a87e1cd38758a4136afd8fa7
Ogbaudu Oghenemaro <maroprosper14@gmail.com>
2022-09-02 16:57:23 +0100

    Completed Eraser and Grayscale feature

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ for(let i = 0; i < size; i++){
 }
 let background = document.getElementById('bgColor');
 background.addEventListener('input' , (e) => {
-    sketch.style.backgroundColor = `${background.value}`;
+    sketch.style.backgroundColor = background.value;
 });
 let options = document.querySelectorAll('input[type="radio"]');
 for(let option of options){
@@ -48,6 +48,30 @@ for(let option of options){
         });
 });
     break;
+    case option.checked && option.id === "grayscale":
+        function getGrey(){
+        let gray = ['#080808','#101010','#181818','#202020','#282828','#303030',
+    '#383838','#404040','#484848','#505050','#585858','#606060','#686868','#707070','#787878',
+'#808080','#888888','#909090','#989898','#A0A0A0','#A8A8A8','#B0B0B0','#BEBEBE','#C0C0C0','#C8C8C8',
+'#D0D0D0','#D3D3D3','#D8D8D8','#DCDCDC','#E0E0E0','#E8E8E8','#F0F0F0','#F5F5F5','#F8F8F8'];
+            color = gray[Math.floor(Math.random()*gray.length)];
+            return color;
+        }
+        let boxes = document.querySelectorAll('.box');
+        boxes.forEach(box => {box.addEventListener('mouseover', (e) =>  {
+                  e.target.style.backgroundColor = getGrey();
+            });
+    });
+        break;
+        case option.checked && option.id === "eraser":
+            background.addEventListener('input' , (e) => {
+                color = background.value;
+                etch(color);
+            });
+            etch(color);
+        break;
+        case option.checked && option.id === "lighten":
+            
       }      
    });
 }
